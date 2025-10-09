1b4c5b31f902fa36d3ff064b86762da1c5929898
Bikas <magarbikash1002@gmail.com>
2025-10-10 00:03:59 +0545

    add path in urls

---
+++ blog/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import CategoryViewSet, PostViewSet, CommentViewSet, UserViewSet
+
+router = DefaultRouter()
+router.register(r'categories', CategoryViewSet)
+router.register(r'posts', PostViewSet)
+router.register(r'comments', CommentViewSet)
+router.register(r'users', UserViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
