b22d4ac4f594307c48953b74e3733cdcbd08d788
Bikas <magarbikash1002@gmail.com>
2025-10-10 00:00:17 +0545

    create views and viewssets

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,61 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django.contrib.auth.models import User
+from .models import Category, Post, Comment, Like
+from .serializers import *
 
-# Create your views here.
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.filter(is_published=True)
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def get_serializer_class(self):
+        if self.action in ['create', 'update', 'partial_update']:
+            return PostCreateSerializer
+        return PostSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+    # Custom action: GET /api/posts/{id}/comments/
+    @action(detail=True, methods=['get'])
+    def comments(self, request, pk=None):
+        post = self.get_object()
+        comments = post.comments.all()
+        serializer = CommentSerializer(comments, many=True)
+        return Response(serializer.data)
+
+    # Custom action: POST /api/posts/{id}/like/
+    @action(detail=True, methods=['post', 'delete'])
+    def like(self, request, pk=None):
+        post = self.get_object()
+        
+        if request.method == 'POST':
+            like, created = Like.objects.get_or_create(post=post, user=request.user)
+            if created:
+                return Response({'status': 'liked'}, status=status.HTTP_201_CREATED)
+            return Response({'status': 'already liked'})
+        
+        elif request.method == 'DELETE':
+            like = Like.objects.filter(post=post, user=request.user)
+            if like.exists():
+                like.delete()
+                return Response({'status': 'unliked'})
+            return Response({'status': 'not liked'}, status=status.HTTP_400_BAD_REQUEST)
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
